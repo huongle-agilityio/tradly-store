cc274025a3a3bd5e9cea00bdc6228a22b8643dfa
HuongLe <huong.le@asnet.com.vn>
2025-02-28 12:06:52 +0700

    #2 Remove redundant ThemeProvider

--- practice-one/src/app/_layout.tsx
+++ practice-one/src/app/_layout.tsx
@@ -1,14 +1,8 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
 import 'react-native-reanimated';
 
 export {
@@ -46,14 +40,8 @@ export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-      </Stack>
-    </ThemeProvider>
-  );
-}
+const RootLayoutNav = () => (
+  <Stack>
+    <Stack.Screen name="index" options={{ headerShown: false }} />
+  </Stack>
+);
