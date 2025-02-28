6ca9698289bfae237ee81404a3006b001e8333ac
HuongLe <huong.le@asnet.com.vn>
2025-02-28 13:39:22 +0700

    #4 Define common variables

---
+++ practice-one/src/ui/themes/colors.ts
@@ -0,0 +1,95 @@
+const greenPallet = {
+  green1: '#33907c',
+  green2: '#13b58c',
+  green3: '#277350',
+} as const;
+
+const redPallet = {
+  red1: '#901F2E',
+  red2: '#ff5880',
+  red3: '#E4ABB1',
+} as const;
+
+const whitePallet = {
+  white1: '#fff',
+  whiteBlue1: '#f6f9ff',
+} as const;
+
+const greyPallet = {
+  grey1: '#4f4f4f',
+  grey2: '#4a4a4a',
+  grey3: '#dbdbde',
+  greyTranslucent: 'rgba(8, 11, 18, 0.4)',
+} as const;
+
+const blackPallet = {
+  black1: '#000',
+  black2: '#212121',
+  black3: '#333A42',
+} as const;
+
+const baseColors = {
+  ...greenPallet,
+  ...redPallet,
+  ...whitePallet,
+  ...greyPallet,
+  ...blackPallet,
+} as const;
+
+export const colors = {
+  primary: baseColors.green1,
+  secondary: baseColors.grey1,
+  tertiary: baseColors.whiteBlue1,
+  light: baseColors.white1,
+  placeholder: baseColors.grey1,
+  error: baseColors.red2,
+  success: baseColors.green2,
+  text: {
+    default: baseColors.black1,
+    light: baseColors.white1,
+    primary: baseColors.green2,
+    secondary: baseColors.green1,
+    tertiary: baseColors.grey2,
+    quaternary: baseColors.black2,
+    placeholder: baseColors.grey1,
+    error: baseColors.red2,
+    success: baseColors.green3,
+  },
+  button: {
+    backgroundPrimary: baseColors.green1,
+    backgroundSecondary: baseColors.white1,
+    textPrimary: baseColors.green2,
+    textSecondary: baseColors.white1,
+  },
+  input: {
+    borderPrimary: baseColors.white1,
+    borderSecondary: baseColors.grey3,
+    textPrimary: baseColors.white1,
+    textSecondary: baseColors.grey1,
+    textTertiary: baseColors.black3,
+  },
+  productCard: {
+    background: baseColors.white1,
+    border: baseColors.grey3,
+    textSecondary: baseColors.grey1,
+    textPrimary: baseColors.grey2,
+    textTertiary: baseColors.green1,
+  },
+  storeCard: {
+    background: baseColors.white1,
+    text: baseColors.grey2,
+  },
+  categoryCard: {
+    background: baseColors.greyTranslucent,
+    text: baseColors.white1,
+  },
+  tabItem: {
+    textPrimary: baseColors.green1,
+    textSecondary: baseColors.grey1,
+  },
+  cartItem: {
+    background: baseColors.white1,
+    textPrimary: baseColors.green1,
+    textSecondary: baseColors.grey1,
+  },
+} as const;
