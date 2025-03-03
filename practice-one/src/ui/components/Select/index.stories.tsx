3c6064648613faf4622c72199f8eee7b0422a3e0
HuongLe <huong.le@asnet.com.vn>
2025-03-03 17:14:36 +0700

    #8 Implement select component, unit test and storybook

---
+++ practice-one/src/ui/components/Select/index.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+
+// Components
+import { Select } from '.';
+
+// Themes
+import { CART_QUANTITY } from '@/mocks';
+import { useState } from 'react';
+
+const meta = {
+  title: 'Select',
+  component: Select,
+  args: {
+    value: '1',
+    options: CART_QUANTITY,
+    onValueChange: fn(),
+    style: {
+      width: 100,
+    },
+  },
+} satisfies Meta<typeof Select>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const SelectWrapper = (props: any) => {
+  const [value, setValue] = useState('1');
+
+  return <Select {...props} value={value} onValueChange={setValue} />;
+};
+
+export const Default: Story = {
+  render: (args) => <SelectWrapper {...args} />,
+  args: {},
+};
+
+export const withError: Story = {
+  render: (args) => <SelectWrapper {...args} />,
+  args: {
+    error: 'This field is required',
+  },
+};
+
+export const Disabled: Story = {
+  render: (args) => <SelectWrapper {...args} />,
+  args: {
+    disabled: true,
+  },
+};
--- practice-one/src/ui/icons/index.ts
+++ practice-one/src/ui/icons/index.ts
@@ -1,3 +1,4 @@
 export * from './SvgFactory';
 export * from './StoreIcon';
 export * from './SearchIcon';
+export * from './ArrowDownIcon';
