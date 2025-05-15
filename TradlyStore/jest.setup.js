87bd266444a23d8e25f05eff4620b910f6f51e37
HuongLe <huong.le@asnet.com.vn>
2025-05-15 16:15:58 +0700

    #120 Add mocks for react-native-bootsplash and Firebase Crashlytics in Jest setup

--- TradlyStore/jest.setup.js
+++ TradlyStore/jest.setup.js
@@ -9,6 +9,13 @@ jest.mock('@gorhom/portal', () => {
   };
 });
 
+jest.mock('react-native-bootsplash', () => {
+  return {
+    hide: jest.fn().mockResolvedValueOnce(),
+    getVisibilityStatus: jest.fn().mockResolvedValue('hidden'),
+  };
+});
+
 // Mock @gorhom/bottom-sheet
 jest.mock('@gorhom/bottom-sheet', () => {
   const React = require('react');
@@ -90,6 +97,15 @@ jest.mock('@react-native-firebase/app', () => {
     },
   };
 });
+
+jest.mock('@react-native-firebase/crashlytics', () => ({
+  crashlytics: () => ({
+    log: jest.fn(),
+    recordError: jest.fn(),
+    setUserId: jest.fn(),
+    setAttributes: jest.fn(),
+  }),
+}));
 
 jest.mock('@react-native-firebase/messaging', () => {
   return Object.assign(
