5d47e55668f99d48e1940e78e060209bf8ed5071
HuongLe <huong.le@asnet.com.vn>
2025-05-15 16:05:47 +0700

    #120 Update animation swipe to delete cart item

--- TradlyStore/src/components/common/CartItem/index.tsx
+++ TradlyStore/src/components/common/CartItem/index.tsx
@@ -1,5 +1,12 @@
 import { memo, useMemo } from 'react';
 import { useTheme } from '@react-navigation/native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 import { Image, TouchableOpacity, View, StyleSheet } from 'react-native';
 
 // Components
@@ -7,13 +14,16 @@ import { Text } from '../Text';
 import { Dropdown } from '../Dropdown';
 
 // Icons
-import { ArrowDownIcon } from '@/components/icons';
+import { ArrowDownIcon, TrashIcon } from '@/components/icons';
 
 // Stores
 import { useThemeStore } from '@/stores';
 
 // Mocks
 import { CART_QUANTITY } from '@/mocks';
+
+// Hooks
+import { useMedia } from '@/hooks';
 
 // Themes
 import { spacing } from '@/themes';
@@ -43,9 +53,19 @@ export const CartItem = memo(
     onRemoveItem,
     onUpdateQuantityItem,
   }: CartItemProps) => {
+    const { width } = useMedia();
     const { colors } = useTheme();
+
+    // CartItem height
+    const opacity = useSharedValue(1);
+    const itemHeight = useSharedValue(205);
+    const marginVertical = useSharedValue(10);
+    const translationX = useSharedValue(0);
+
+    // Store
     const isDark = useThemeStore((state) => state.isDark);
 
+    const TRANSLATION_OUT_SCREEN_X = -width * 0.7;
     const stylesDynamic = useMemo(
       () =>
         StyleSheet.create({
@@ -56,8 +76,8 @@ export const CartItem = memo(
               : { paddingVertical: 25 }),
           },
           cartWrapper: {
-            backgroundColor: colors.backgroundSecondary,
-            elevation: 2,
+            backgroundColor: colors.light,
+            elevation: 5,
           },
           buttonBorder: {
             borderTopColor: colors.productCard.border,
@@ -74,80 +94,136 @@ export const CartItem = memo(
       onUpdateQuantityItem?.(id, value);
     };
 
+    const panGesture = Gesture.Pan()
+      .minVelocityX(-20)
+      .onUpdate((e) => {
+        if (e.translationX > 0) return;
+        translationX.value = e.translationX;
+      })
+      .onEnd(() => {
+        const shouldDismiss = translationX.value < TRANSLATION_OUT_SCREEN_X;
+        if (shouldDismiss) {
+          translationX.value = withTiming(-width);
+          itemHeight.value = withTiming(0);
+          marginVertical.value = withTiming(0);
+          opacity.value = withTiming(0);
+          runOnJS(handleRemoveItem)();
+        } else {
+          translationX.value = withTiming(0);
+        }
+      });
+
+    const animatedStyles = useAnimatedStyle(() => ({
+      transform: [{ translateX: translationX.value }],
+    }));
+
+    const animatedIconStyles = useAnimatedStyle(() => ({
+      opacity: withTiming(
+        translationX.value < -(width / 2) ? opacity.value : 0,
+        {
+          duration: 200,
+        },
+      ),
+    }));
+
+    const animatedHeight = useAnimatedStyle(() => ({
+      height: itemHeight.value,
+    }));
+
+    const animatedVertical = useAnimatedStyle(() => ({
+      marginVertical: marginVertical.value,
+      opacity: opacity.value,
+    }));
+
     return (
-      <View style={stylesDynamic.cartWrapper}>
-        <View style={[[styles.content, stylesDynamic.gapContent]]}>
-          <Image
-            source={{
-              uri: image,
-            }}
-            accessibilityRole="image"
-            accessibilityLabel={`image of ${name} product`}
-            style={{
-              width: spacing[30],
-              height: spacing[30],
-              borderRadius: spacing['2.5'],
-            }}
-          />
-
-          <View style={styles.textWrapper}>
-            <Text fontWeight="normal" color="placeholder">
-              {name}
-            </Text>
-
-            <View style={styles.priceWrapper}>
-              <Text fontWeight="bold" color="secondary" fontSize="lg">
-                ${calculateDiscountedPrice(price, discount)}
-              </Text>
-              {!!discount && (
-                <View style={styles.discountWrapper}>
-                  <Text
-                    fontWeight="normal"
-                    color="placeholder"
-                    textStyle={styles.discountText}
-                  >
-                    ${price}
-                  </Text>
-                  <Text fontWeight="normal" color="placeholder">
-                    {discount}% off
-                  </Text>
-                </View>
-              )}
-            </View>
-
-            <View style={styles.quantityWrapper}>
-              <Text fontSize="sm" fontWeight="normal" color="placeholder">
-                Qty:
-              </Text>
-              <Dropdown
-                options={CART_QUANTITY}
-                value={quantity.toString()}
-                disabled={!onUpdateQuantityItem}
-                onChange={handleUpdateQuantity}
-                renderRightIcon={
-                  <ArrowDownIcon
-                    size={10}
-                    {...(isDark && { color: colors.input.textPlaceholder })}
-                  />
-                }
-                style={{ width: 60 }}
+      <GestureDetector gesture={panGesture}>
+        <Animated.View style={[stylesDynamic.cartWrapper, animatedVertical]}>
+          <Animated.View style={[animatedStyles, animatedHeight]}>
+            <View style={[styles.content, stylesDynamic.gapContent]}>
+              <Image
+                source={{
+                  uri: image,
+                }}
+                accessibilityRole="image"
+                accessibilityLabel={`image of ${name} product`}
+                style={{
+                  width: spacing[30],
+                  height: spacing[30],
+                  borderRadius: spacing['2.5'],
+                }}
               />
-            </View>
-          </View>
-        </View>
 
-        {onRemoveItem && (
-          <TouchableOpacity
-            accessibilityRole="button"
-            style={[styles.buttonWrapper, stylesDynamic.buttonBorder]}
-            onPress={handleRemoveItem}
-          >
-            <Text fontWeight="normal" color="placeholder">
-              Remove
-            </Text>
-          </TouchableOpacity>
-        )}
-      </View>
+              <View style={styles.textWrapper}>
+                <Text fontWeight="normal" color="placeholder">
+                  {name}
+                </Text>
+
+                <View style={styles.priceWrapper}>
+                  <Text fontWeight="bold" color="secondary" fontSize="lg">
+                    ${calculateDiscountedPrice(price, discount)}
+                  </Text>
+                  {!!discount && (
+                    <View style={styles.discountWrapper}>
+                      <Text
+                        fontWeight="normal"
+                        color="placeholder"
+                        textStyle={styles.discountText}
+                      >
+                        ${price}
+                      </Text>
+                      <Text fontWeight="normal" color="placeholder">
+                        {discount}% off
+                      </Text>
+                    </View>
+                  )}
+                </View>
+
+                <View style={styles.quantityWrapper}>
+                  <Text fontSize="sm" fontWeight="normal" color="placeholder">
+                    Qty:
+                  </Text>
+                  <Dropdown
+                    options={CART_QUANTITY}
+                    value={quantity.toString()}
+                    disabled={!onUpdateQuantityItem}
+                    onChange={handleUpdateQuantity}
+                    renderRightIcon={
+                      <ArrowDownIcon
+                        size={10}
+                        {...(isDark && {
+                          color: colors.input.textPlaceholder,
+                        })}
+                      />
+                    }
+                    style={{ width: 60 }}
+                  />
+                </View>
+              </View>
+            </View>
+            {onRemoveItem && (
+              <TouchableOpacity
+                accessibilityRole="button"
+                style={[styles.buttonWrapper, stylesDynamic.buttonBorder]}
+                onPress={handleRemoveItem}
+              >
+                <Text fontWeight="normal" color="placeholder">
+                  Remove
+                </Text>
+              </TouchableOpacity>
+            )}
+          </Animated.View>
+
+          <Animated.View style={[styles.deleteIcon, animatedIconStyles]}>
+            <View style={styles.deleteIconWrapper} />
+            <TrashIcon
+              width={40}
+              height={40}
+              color={colors.error}
+              style={styles.icon}
+            />
+          </Animated.View>
+        </Animated.View>
+      </GestureDetector>
     );
   },
 );
@@ -174,4 +250,22 @@ const styles = StyleSheet.create({
   discountText: {
     textDecorationLine: 'line-through',
   },
+  deleteIcon: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: -1,
+  },
+  deleteIconWrapper: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'red',
+    position: 'absolute',
+    opacity: 0.2,
+  },
+  icon: { alignSelf: 'flex-end', marginRight: 40 },
 });
