b9fe992be1d31e8089a613d6a3de88521f4d748d
HuongLe <huong.le@asnet.com.vn>
2025-05-19 11:25:57 +0700

    #124 Sticky header for product details

--- TradlyStore/src/screens/product/ProductDetail/components/Content/index.tsx
+++ TradlyStore/src/screens/product/ProductDetail/components/Content/index.tsx
@@ -1,28 +1,22 @@
 import { memo, useMemo } from 'react';
 import { useTheme } from '@react-navigation/native';
-import {
-  ScrollView,
-  TouchableOpacity,
-  Image,
-  View,
-  StyleSheet,
-} from 'react-native';
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from 'react-native-reanimated';
+import { Image, View, StyleSheet } from 'react-native';
 
 // Components
+import { Header } from '../Header';
 import { StickyFooter } from '@/components/shared';
 import { ProductInformation } from '../ProductInformation';
-import { Button, ProductCarousel, Skeleton, Text } from '@/components/common';
-
-// Icons
-import {
-  ArrowLeftIcon,
-  MenuDotIcon,
-  OutlineHeartIcon,
-  ShareIcon,
-} from '@/components/icons';
+import { Button, Skeleton, Text } from '@/components/common';
 
 // Interfaces
 import { Store } from '@/interfaces';
+
+// Stores
+import { useThemeStore } from '@/stores';
 
 // Themes
 import { lineHeights, radius, spacing } from '@/themes';
@@ -31,7 +25,6 @@ import { lineHeights, radius, spacing } from '@/themes';
 import { calculateDiscountedPrice, getProductDetails } from '@/utils';
 
 interface ContentProps {
-  isLoading: boolean;
   price: number;
   quantity: number;
   title: string;
@@ -39,6 +32,7 @@ interface ContentProps {
   category: string;
   location: string;
   discount?: number;
+  isLoading?: boolean;
   slideImages: string[];
   description?: string;
   store?: Store;
@@ -63,6 +57,10 @@ export const Content = memo(
     store,
   }: ContentProps) => {
     const { colors } = useTheme();
+    const scrollY = useSharedValue(0);
+    const opacity = useSharedValue(1);
+
+    const isDark = useThemeStore((state) => state.isDark);
 
     const productInfo = getProductDetails({ priceType, category, location });
     const productAdditional = [
@@ -76,13 +74,10 @@ export const Content = memo(
     const stylesDynamic = useMemo(
       () =>
         StyleSheet.create({
-          backgroundIcon: {
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            backgroundColor: colors.backgroundSecondary,
-            borderRadius: radius.full,
-            opacity: 0.5,
+          contentContainerStyle: {
+            backgroundColor: isDark
+              ? colors.backgroundSecondary
+              : colors.tertiary,
           },
           contentWrapper: {
             borderRadius: radius.lg,
@@ -95,20 +90,16 @@ export const Content = memo(
             paddingVertical: 2,
             paddingHorizontal: 6,
           },
-          icon: {
-            position: 'relative',
-            width: 32,
-            height: 32,
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderRadius: radius.full,
-            backgroundColor: colors.backgroundOpacity,
-            borderWidth: 1,
-            borderColor: colors.light,
-          },
         }),
-      [colors],
+      [colors.backgroundSecondary, colors.error, colors.tertiary, isDark],
     );
+
+    const scrollHandler = useAnimatedScrollHandler({
+      onScroll: (event) => {
+        scrollY.value = event.contentOffset.y;
+        opacity.value = event.contentOffset.y;
+      },
+    });
 
     return (
       <StickyFooter
@@ -117,103 +108,75 @@ export const Content = memo(
         buttonText="Add to Cart"
         onPress={handleAddToCart}
       >
-        <ScrollView contentContainerStyle={styles.contentContainerStyle}>
-          <View>
-            <View style={styles.headerWrapper}>
-              <ProductCarousel images={slideImages} />
-
-              <View style={styles.header}>
-                <TouchableOpacity
-                  accessibilityRole="button"
-                  style={stylesDynamic.icon}
-                  onPress={handleBack}
-                >
-                  <ArrowLeftIcon size={18} color={colors.light} />
-                  <View style={stylesDynamic.backgroundIcon} />
-                </TouchableOpacity>
-
-                <View style={styles.iconWrapper}>
-                  <View style={stylesDynamic.icon}>
-                    <ShareIcon size={18} color={colors.light} />
-                    <View style={stylesDynamic.backgroundIcon} />
-                  </View>
-                  <View style={stylesDynamic.icon}>
-                    <OutlineHeartIcon size={18} color={colors.light} />
-                    <View style={stylesDynamic.backgroundIcon} />
-                  </View>
-                  <View style={stylesDynamic.icon}>
-                    <MenuDotIcon size={18} color={colors.light} />
-                    <View style={stylesDynamic.backgroundIcon} />
-                  </View>
-                </View>
+        <Header
+          scrollY={scrollY}
+          opacity={opacity}
+          slideImages={slideImages}
+          handleBack={handleBack}
+        />
+        <Animated.ScrollView
+          scrollEventThrottle={16}
+          showsVerticalScrollIndicator={false}
+          onScroll={scrollHandler}
+          contentContainerStyle={[
+            styles.contentContainerStyle,
+            stylesDynamic.contentContainerStyle,
+          ]}
+        >
+          <View style={[styles.productWrapper, stylesDynamic.contentWrapper]}>
+            <Text
+              color="placeholder"
+              fontSize="lg"
+              fontWeight="bold"
+              textStyle={{ lineHeight: lineHeights.md }}
+            >
+              {title}
+            </Text>
+            {isLoading ? (
+              <View style={styles.loading}>
+                <Skeleton width="40%" height={20} borderRadius={4} />
               </View>
-            </View>
-
-            <View style={[styles.productWrapper, stylesDynamic.contentWrapper]}>
-              {isLoading ? (
-                <View style={styles.loading}>
-                  <Skeleton width="60%" height={20} borderRadius={4} />
-                  <Skeleton width="40%" height={20} borderRadius={4} />
-                </View>
-              ) : (
-                <>
-                  <Text
-                    color="placeholder"
-                    fontSize="lg"
-                    fontWeight="bold"
-                    textStyle={{ lineHeight: lineHeights.md }}
-                  >
-                    {title}
+            ) : (
+              <>
+                <View style={styles.price}>
+                  <Text fontSize="lg" fontWeight="bold" color="secondary">
+                    ${calculateDiscountedPrice(price, discount)}
                   </Text>
-                  <View style={styles.price}>
-                    <Text fontSize="lg" fontWeight="bold" color="secondary">
-                      ${calculateDiscountedPrice(price, discount)}
-                    </Text>
-                    {!!discount && (
-                      <>
-                        <Text
-                          fontWeight="normal"
-                          color="placeholder"
-                          textStyle={styles.textPrice}
-                        >
-                          ${price}
-                        </Text>
-                        <Text fontWeight="normal" color="placeholder">
-                          {discount}% off
-                        </Text>
-                      </>
-                    )}
-                    {isSoldOut && (
-                      <Text color="light" textStyle={stylesDynamic.banner}>
-                        SOLD OUT
+                  {!!discount && (
+                    <>
+                      <Text
+                        fontWeight="normal"
+                        color="placeholder"
+                        textStyle={styles.textPrice}
+                      >
+                        ${price}
                       </Text>
-                    )}
-                  </View>
-                </>
-              )}
-            </View>
+                      <Text fontWeight="normal" color="placeholder">
+                        {discount}% off
+                      </Text>
+                    </>
+                  )}
+                  {isSoldOut && (
+                    <Text color="light" textStyle={stylesDynamic.banner}>
+                      SOLD OUT
+                    </Text>
+                  )}
+                </View>
+              </>
+            )}
           </View>
 
           <View style={[styles.storeWrapper, stylesDynamic.contentWrapper]}>
             <View style={styles.storeTitle}>
-              {isLoading ? (
-                <>
-                  <Skeleton width={32} height={32} style={styles.image} />
-                  <Skeleton width={100} height={20} borderRadius={4} />
-                </>
-              ) : (
-                <>
-                  <Image
-                    accessibilityRole="image"
-                    source={{ uri: store?.image }}
-                    style={styles.image}
-                    accessibilityLabel={`avatar of ${store?.username} store`}
-                  />
-                  <Text fontWeight="normal" color="placeholder">
-                    {store?.username}
-                  </Text>
-                </>
-              )}
+              <Image
+                accessibilityRole="image"
+                source={{ uri: store?.image }}
+                style={styles.image}
+                accessibilityLabel={`avatar of ${store?.username} store`}
+              />
+              <Text fontWeight="normal" color="placeholder">
+                {store?.username}
+              </Text>
             </View>
 
             <View style={styles.button}>
@@ -232,7 +195,8 @@ export const Content = memo(
                 color="placeholder"
                 textStyle={{ lineHeight: lineHeights.md }}
               >
-                {description}
+                {description} {description} {description} {description}
+                {description} {description} {description} {description}
               </Text>
             )}
           </View>
@@ -266,7 +230,7 @@ export const Content = memo(
               }}
             />
           </View>
-        </ScrollView>
+        </Animated.ScrollView>
       </StickyFooter>
     );
   },
@@ -282,15 +246,6 @@ export const styles = StyleSheet.create({
     paddingBottom: spacing['7.5'],
     paddingHorizontal: spacing[4],
   },
-  header: {
-    padding: spacing[4],
-    width: '100%',
-    flexDirection: 'row',
-    position: 'absolute',
-    justifyContent: 'space-between',
-    top: 0,
-  },
-
   price: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -321,9 +276,7 @@ export const styles = StyleSheet.create({
     marginRight: 5,
     textDecorationLine: 'line-through',
   },
-  headerWrapper: { height: 226, position: 'relative' },
-  iconWrapper: { flexDirection: 'row', gap: 13 },
-  loading: { gap: 10 },
+  loading: { marginTop: 10 },
   image: { width: 32, height: 32, borderRadius: radius.full },
   button: { height: 23 },
 });
