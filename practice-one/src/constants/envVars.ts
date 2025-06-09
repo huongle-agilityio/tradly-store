6f8620c1fcce2cc71d668630bf78f4f494742d0f
HuongLe <huong.le@asnet.com.vn>
2025-06-09 16:03:16 +0700

    Add API_URL to app.config and update BASE_API handling in envVars

--- practice-one/src/constants/envVars.ts
+++ practice-one/src/constants/envVars.ts
@@ -1,1 +1,4 @@
-export const BASE_API = process.env.EXPO_PUBLIC_API_URL;
+import Constants from 'expo-constants';
+
+export const BASE_API =
+  process.env.EXPO_PUBLIC_API_URL ?? Constants.expoConfig?.extra?.API_URL;
